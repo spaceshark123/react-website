5fe501b4882ca66309f9c1e224160b197615bd10
spaceshark123 <amogh.patnaik@icloud.com>
2024-09-09 22:27:48 -0500

    made 500 buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,9 +16,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {Array.from({ length: 504 }, (_, i) => (
+          <Button key={i} />
+        ))}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
